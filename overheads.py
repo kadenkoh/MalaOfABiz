c589b4b8c0eba0b8d5662d897cf1dc06169192cd
kadenkoh <s10255912@connect.np.edu.sg>
2024-01-24 20:48:53 +0800

    open csv file and read

---
+++ overheads.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import csv
+
+#Creating file path to the cash on hand file
+fp = Path.cwd()/"csv_reports"/"Overheads.csv"
+#Checking if file path exists
+print(fp.exists()) 
+
+# read the csv file.
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader) # skip header
