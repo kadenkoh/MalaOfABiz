19ead6e32ffa98b1d210ab6aa507d8e945ad0cd6
kadenkoh <s10255912@connect.np.edu.sg>
2024-01-24 20:57:37 +0800

    wrote the function for overhead

--- overheads.py
+++ overheads.py
@@ -10,3 +10,17 @@ print(fp.exists())
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader) # skip header
+
+    #setting up the variables 
+    highest_overhead_amount= -0.1 #set the highest overhead amount as -0.1 to allow for all overhead values to be cycled through
+
+    # read the data from each row
+    for row in reader:
+            category = row[0]
+            overhead_amount = float(row[1])
+            #loop through all the overhead amounts in the csv file till the largest amount is captured and no other values can be greater than that and the loop stops and presents the highest overhead amount
+            if overhead_amount > highest_overhead_amount:
+                highest_overhead_amount=overhead_amount
+                highest_overhead_category=category
+                
+    print(f'The highest overhead expense category is {highest_overhead_category} at an amount of {highest_overhead_amount}.')
