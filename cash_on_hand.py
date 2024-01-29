4c899d3a4cc2678c2623a97f63f174296b3ee639
Verve1 <Oliverchewsg@gmail.com>
2024-01-29 13:42:38 +0800

    wrote the increment and decrement code and also wrote the difference calculation

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -32,3 +32,35 @@ def cash_on_hand_function():
         # Create lists for highest increment and decrement with 0 values to allow for loop function to run through
         highest_increment = ['day', 0]
         highest_decrement = ['day', 0]
+
+        # Process the first data row to set the initial previous cash on hand
+        first_row_of_data= next(reader)
+        previous_cash_on_hand = float(first_row_of_data[1])
+
+        # Loop through the rows to calculate daily cash on hand differences
+        for row in reader:
+            day = row[0]
+            current_cash_on_hand = float(row[1])
+            # Calculate the difference and append it to the list 
+            difference = current_cash_on_hand - previous_cash_on_hand
+            difference_in_cash_on_hand.append([day, difference])  
+
+            # Update previous cash on hand with current cash on hand
+            previous_cash_on_hand = current_cash_on_hand
+
+            # Identifying the deficits which are negative differences
+            if difference < 0:
+                # Append all deficits into a list 
+                total_deficit.append([day, difference])
+
+        # Calculating the highest increment and decrement of cash on hand
+        for day, difference in difference_in_cash_on_hand:
+            
+            # Checking if the current difference is less than the current highest decrement
+            # If it is, this becomes the new highest decrement
+            if difference < highest_decrement[1]:
+                highest_decrement=[day, difference]
+            # Checking if the current difference is greater than the current highest increment
+            # If it is, this becomes the new highest increment
+            elif difference > highest_increment[1]:
+                highest_increment=[day, difference]
