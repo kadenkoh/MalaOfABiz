f721ecd8b3f30cdd6d82e5a0f00e96477f69712c
kadenkoh <s10255912@connect.np.edu.sg>
2024-01-29 13:28:42 +0800

    first part of cash on hand

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,12 +1,34 @@
 from pathlib import Path
 import csv
 
-#Creating file path to the cash on hand file
-fp = Path.cwd()/"csv_reports"/"increasing.csv"
-#Checking if file path exists
-print(fp.exists()) 
+def cash_on_hand_function():
+    """
+    - This function detects three types of trends in data: increasing, decreasing, and fluctuating. 
+      For increasing trends it calculates the highest increment in cash on hand and the day it happened
+      While for decreasing trends, it calculates the highest decrement in cash on hand and the day it happened
+      When faced with the fluctuating data trend the function will calculate the top 3 highest cash on hand deficits and also calculate all the cash on hand deficits
 
-# read the csv file.
-with fp.open(mode="r", encoding="UTF-8", newline="") as file:
-    reader = csv.reader(file)
-    next(reader) # skip header
+    - No parameters are needed 
+    """
+    #Creating file path to the cash on hand file
+    fp = Path.cwd()/"csv_reports"/"Cash_on_Hand.csv"
+    #Checking if file path exists
+    print(fp.exists()) 
+
+    # read the csv file.
+    with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+        reader = csv.reader(file)
+        next(reader) # skip header
+
+        # Create empty list to put the difference data and all the deficit values
+        difference_in_cash_on_hand = []
+        total_deficit=[]
+
+        # Create lists for the top 3 deficits with 0 deficit values to allow for loop function to run through
+        first_deficit=['day', 0]
+        second_deficit=['day', 0]
+        third_deficit=['day', 0]
+
+        # Create lists for highest increment and decrement with 0 values to allow for loop function to run through
+        highest_increment = ['day', 0]
+        highest_decrement = ['day', 0]
